2887a2a536351c917f85f5e51ea5d83f087eec3e
shaked <shaked1hen@gmail.com>
2020-08-04 17:00:37 +0300

    added style to the item

--- lib/widget/word_list_item.dart
+++ lib/widget/word_list_item.dart
@@ -3,14 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:word_me/models/heb_word.dart';
 
 
-class WordListItem extends StatelessWidget {
+class WordListItem extends StatefulWidget {
   final HebWord word;
   WordListItem(this.word);
+
+  @override
+  _WordListItemState createState() => _WordListItemState();
+}
+
+class _WordListItemState extends State<WordListItem> {
+  bool isOpen = false;
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
-        title: Text('${word.word}'),
+        leading: const Icon(Icons.comment),
+        title: Text('${widget.word.word}'),
+        subtitle: Text(
+          isOpen ? widget.word.translation : '',
+          style: TextStyle(
+            fontSize: isOpen ? 20 : 0,
+            fontWeight: FontWeight.normal,
+          )
+        ),
+        onTap: () {
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
+        onLongPress: () {
+          // TODO: change to move to explain button
+          print('click onLongPress');
+        },
       ),
     );
   }
