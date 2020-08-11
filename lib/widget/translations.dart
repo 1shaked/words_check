1794d3d925a8b3674f2e48e49e1bc0a0a8be7f72
shaked <shaked1hen@gmail.com>
2020-08-11 21:40:41 +0300

    added the translation widjet

---
+++ lib/widget/translations.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Translations extends StatelessWidget {
+  final List<String> translations;
+
+  Translations(this.translations);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // height: 50,
+      width: 50,
+      child: ListView.builder(
+        itemCount: translations.length,
+        itemBuilder: (context , index) {
+          int number = index + 1;
+          return Text('${number}) ${translations[index]}');
+        },
+        shrinkWrap: true
+        // separatorBuilder: (_ , index) => Divider(),
+      ),
+    );
+  }
+}
