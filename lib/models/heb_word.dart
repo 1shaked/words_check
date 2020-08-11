25bb4b6ff927f8ccc104f9523b36d6d15bfe3086
shaked <shaked1hen@gmail.com>
2020-08-11 21:40:19 +0300

    added the get translation

--- lib/models/heb_word.dart
+++ lib/models/heb_word.dart
@@ -4,4 +4,24 @@ class HebWord {
   String example;
   int score;
   HebWord(this.word, this.translation, this.example, this.score);
+
+  List<String> get translationsList => splitText(translation);
+  List<String> get exampleList => splitText(example);
+  
+
+  List<String> splitText (String text) {
+    if (!text.contains('.')) {
+      return [text];
+    }
+    // return ['text' , 't2'];
+    List<String> textSplited =  text.split('.');
+    for (int i = 0; i < textSplited.length; i++) {
+      if (textSplited[i].length == 0) continue;
+      if ('0123456789'.contains(textSplited[i][0])) {
+        String firstChar = textSplited[i][0];
+        textSplited[i].replaceFirst(firstChar, ' ');
+      }
+    }
+    return textSplited;    
+  }
 }
