d3d1fcf4af2c94fd0e68be3200b41bbaf9a85b4c
shaked <shaked1hen@gmail.com>
2020-08-11 21:46:11 +0300

    added the word list ite, widhet

--- lib/widget/word_list_item.dart
+++ lib/widget/word_list_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 import 'package:word_me/models/heb_word.dart';
+import 'package:word_me/widget/WordUses.dart';
+import 'package:word_me/widget/translations.dart';
 
 
 class WordListItem extends StatefulWidget {
@@ -13,6 +15,46 @@ class WordListItem extends StatefulWidget {
 
 class _WordListItemState extends State<WordListItem> {
   bool isOpen = false;
+
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('${widget.word.word}'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Translations(widget.word.translationsList),
+                WordUses(widget.word.exampleList),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('ידעתי'),
+              color: Colors.green[700],
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('לא ידעתי'),
+              color: Colors.red[400],
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,9 +75,11 @@ class _WordListItemState extends State<WordListItem> {
         },
         onLongPress: () {
           // TODO: change to move to explain button
-          print('click onLongPress');
+          print('long press');
+          _showMyDialog();
         },
       ),
     );
   }
 }
+
