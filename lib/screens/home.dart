193786afae5652f36a3bac643aff191c15f3d4e8
shaked chen <shaked1hen@gmail.com>
2020-08-15 21:23:41 +0300

    created the Drawer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:word_me/models/words_dictionary.dart';
-import 'package:word_me/widget/increment_uses.dart';
 import 'package:word_me/widget/page_conntroller.dart';
-import 'package:word_me/widget/show_uses.dart';
 import 'package:word_me/widget/word_list.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,27 +13,37 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => WordsDictionary()),
-        ],
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('home page!')
-          ),
-          body: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                PageConntroller(),
-                SizedBox(
-                  child: WordList(),
-                  height: 500, 
-                ),
-                
-              ],
-            )      
+      providers: [
+        ChangeNotifierProvider(create: (context) => WordsDictionary()),
+      ],
+      child: Scaffold(
+        appBar: AppBar(title: Text('home page!')),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text("Ttem 1"),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title: Text("Item 2"),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ],
           ),
         ),
+        body: Container(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            PageConntroller(),
+            SizedBox(
+              child: WordList(),
+              height: 500,
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
