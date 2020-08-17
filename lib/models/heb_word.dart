e4eb972f8d8b7bb2893167255a94f8572e8a5641
shaked chen <shaked1hen@gmail.com>
2020-08-17 16:49:09 +0300

    added the known words score

--- lib/models/heb_word.dart
+++ lib/models/heb_word.dart
@@ -1,24 +1,25 @@
-class HebWord { 
+class HebWord {
   String word;
   String translation;
   String example;
   int score;
+  int knownWordScore = -1;
   HebWord(this.word, this.translation, this.example, this.score);
 
   List<String> get translationsList => splitText(translation);
   List<String> get exampleList => splitText(example);
-  
+
   @override
-  String toString () {
+  String toString() {
     return super.toString() + ' ${this.word} ${this.score}';
   }
 
-  List<String> splitText (String text) {
+  List<String> splitText(String text) {
     if (!text.contains('.')) {
       return [text];
     }
     // return ['text' , 't2'];
-    List<String> textSplited =  text.split('.');
+    List<String> textSplited = text.split('.');
     for (int i = 0; i < textSplited.length; i++) {
       if (textSplited[i].length == 0) continue;
       if ('0123456789'.contains(textSplited[i][0])) {
@@ -26,6 +27,6 @@ class HebWord {
         textSplited[i].replaceFirst(firstChar, ' ');
       }
     }
-    return textSplited;    
+    return textSplited;
   }
 }
