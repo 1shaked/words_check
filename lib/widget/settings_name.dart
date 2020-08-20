b11b28d9cd8137adbda3214ed202d7466cddd30d
shaked chen <shaked1hen@gmail.com>
2020-08-20 15:21:59 +0300

    setting the user name

--- lib/widget/settings_name.dart
+++ lib/widget/settings_name.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/models/test_config.dart';
 
 class SettingsName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TestConfig testConfig = Provider.of<TestConfig>(context);
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
-          color: Colors.red, //                   <--- border color
-          width: 5.0,
+          color: Colors.red[100], //                   <--- border color
+          width: 1.0,
         ),
       ),
       padding: EdgeInsets.all(16.0),
-      child: Text('settings name'),
+      child: TextFormField(
+        decoration:
+            InputDecoration(hintText: "Your name is ${testConfig.name}"),
+        // The validator receives the text that the user has entered.
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      ),
     );
   }
 }
