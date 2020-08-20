cdef7daba294ea26209abce69d679fb0e82b01b2
shaked chen <shaked1hen@gmail.com>
2020-08-20 15:21:19 +0300

    adding the current widjets

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:word_me/models/test_config.dart';
 import 'package:word_me/widget/nav_drawer.dart';
+import 'package:word_me/widget/seetings_word_at_day.dart';
 import 'package:word_me/widget/settings_frequency.dart';
 import 'package:word_me/widget/settings_name.dart';
+import 'package:word_me/widget/settings_word_at_test.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -11,6 +13,8 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final _settingsFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -21,13 +25,25 @@ class _SettingsState extends State<Settings> {
         appBar: AppBar(title: Text('Settings')),
         drawer: NavDrawer(),
         body: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // here will be the name
-              Expanded(flex: 5, child: SettingsName()),
-              Expanded(flex: 5, child: SettingsFrequency()),
-            ],
+          child: Form(
+            key: _settingsFormKey,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(flex: 5, child: SettingsWordAtDay()),
+                    Expanded(flex: 5, child: SettingsWordAtTest()),
+                  ],
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Expanded(flex: 5, child: SettingsName()),
+                ]),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Expanded(flex: 5, child: SettingsFrequency()),
+                ]),
+              ],
+            ),
           ),
         ),
       ),
