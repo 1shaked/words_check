8fbc6a3cbb6b530c920aaffb87a9846ea518046f
shaked chen <shaked1hen@gmail.com>
2020-08-20 15:22:31 +0300

    setting the freq widjet

--- lib/widget/settings_frequency.dart
+++ lib/widget/settings_frequency.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/models/test_config.dart';
+import 'package:word_me/widget/settings_frequency_item_picker.dart';
 
 class SettingsFrequency extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('SettingsFrequency'),
+    TestConfig testConfig = Provider.of<TestConfig>(context);
+    return Column(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+              child: SizedBox(
+            height: 300,
+            width: 250,
+            child: ListView.builder(
+              itemCount: testConfig.numberOfTimeADays,
+              itemBuilder: (context, i) {
+                return SettingsFrequencyItemPicker(i);
+              },
+            ),
+          ))
+        ]),
+        RaisedButton.icon(
+            onPressed: () => testConfig.addFrequency(),
+            icon: Icon(Icons.add_alarm),
+            label: Text('add'))
+      ],
     );
   }
 }
