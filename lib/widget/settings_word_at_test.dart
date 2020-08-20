7bc5cf9d1aa02dac4cee5909ce935cd8d0f175f1
shaked chen <shaked1hen@gmail.com>
2020-08-20 15:21:47 +0300

    created the setting word at text widjet

--- lib/widget/settings_word_at_test.dart
+++ lib/widget/settings_word_at_test.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:word_me/models/test_config.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-class SettingsWordAtDay extends StatelessWidget {
+class SettingsWordAtTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TestConfig testConfig = Provider.of<TestConfig>(context);
     return Container(
-      child: Text('settings'),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          WhitelistingTextInputFormatter.digitsOnly
+        ],
+        decoration:
+            InputDecoration(hintText: "wordsAtTest ${testConfig.wordsAtTest}"),
+        validator: (value) {
+          return int.parse(value) < 1
+              ? 'pls enter a value bigger then 0'
+              : null;
+        },
+      ),
     );
   }
 }
