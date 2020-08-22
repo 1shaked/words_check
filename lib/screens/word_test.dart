073deb8885301af881792cfb4f18762c6327642b
shaked chen <shaked1hen@gmail.com>
2020-08-22 18:18:16 +0300

    created the word test page

---
+++ lib/screens/word_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/models/words_dictionary.dart';
+import 'package:word_me/widget/nav_drawer.dart';
+import 'package:word_me/widget/word_test_body.dart';
+
+class WordTest extends StatefulWidget {
+  @override
+  _WordTestState createState() => _WordTestState();
+}
+
+class _WordTestState extends State<WordTest> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => WordsDictionary()),
+      ],
+      child: Scaffold(
+          appBar: AppBar(title: Text('Test ME')),
+          drawer: NavDrawer(),
+          body: WordTestBody()),
+    );
+  }
+}
