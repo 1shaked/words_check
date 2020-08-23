6c7948cd610e9eb29236816bfa19c219009380a9
shaked chen <shaked1hen@gmail.com>
2020-08-23 12:23:37 +0300

    the show answer condition widget

--- lib/widget/word_test/show_answer.dart
+++ lib/widget/word_test/show_answer.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/models/test_maneger.dart';
 
 class ShowAnswer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TestManeger testManeger = Provider.of<TestManeger>(context);
+    return Container(
+      child: testManeger.showTranslation
+          ? showAnswer(testManeger)
+          : showUnknownBox(),
+    );
+  }
+
+  Widget showAnswer(TestManeger testManeger) {
+    return Card(
+        color: Colors.black,
+        child: Container(
+          child: Text(testManeger.currentWord.translation,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 35, color: Colors.white)),
+        ));
+  }
+
+  Widget showUnknownBox() {
+    return Card(
+        color: Colors.black,
+        child: Container(
+          child: Text('What am I ??',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 35, color: Colors.white)),
+        ));
   }
 }
