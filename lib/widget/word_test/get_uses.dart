63e27c7f30f3d0fddfe2a66a287c985c8a5c23eb
shaked chen <shaked1hen@gmail.com>
2020-08-23 12:26:01 +0300

    changed to stf widget need to add the check

--- lib/widget/word_test/get_uses.dart
+++ lib/widget/word_test/get_uses.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/models/test_maneger.dart';
 
-class GetUses extends StatelessWidget {
+class GetUses extends StatefulWidget {
+  @override
+  _GetUsesState createState() => _GetUsesState();
+}
+
+class _GetUsesState extends State<GetUses> {
+  bool didGetHint = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TestManeger testManeger = Provider.of<TestManeger>(context);
+    return Container(
+        margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: showUses(testManeger));
+  }
+
+  Widget needHint() {
+    return Container(
+      child: Container(
+        child: RaisedButton.icon(
+            color: Colors.blueAccent,
+            padding: EdgeInsets.all(15),
+            onPressed: () => print('need help'),
+            icon: Icon(Icons.help_outline, color: Colors.white),
+            label: Text('Get Help!',
+                style: TextStyle(color: Colors.white, fontSize: 20))),
+      ),
+    );
+  }
+
+  Widget showUses(TestManeger maneger) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        color: Colors.redAccent,
+        child: SizedBox(
+          width: double.infinity,
+          height: 60,
+          child: ListView.builder(
+              itemCount: maneger.currentWord.translationsList.length,
+              itemBuilder: (_, index) => Text(
+                  maneger.currentWord.translationsList[index],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+        ));
   }
 }
