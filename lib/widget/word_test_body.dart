9d29152c426afc37f780cab16d2c5a9e29777504
shaked chen <shaked1hen@gmail.com>
2020-08-23 12:24:07 +0300

    added the providor for the test meneger

--- lib/widget/word_test_body.dart
+++ lib/widget/word_test_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:word_me/models/test_config.dart';
+import 'package:word_me/models/test_maneger.dart';
 import 'package:word_me/models/words_dictionary.dart';
 import 'package:word_me/widget/word_test/do_know.dart';
 import 'package:word_me/widget/word_test/get_uses.dart';
@@ -18,28 +20,37 @@ class _WordTestBodyState extends State<WordTestBody> {
   @override
   Widget build(BuildContext context) {
     WordsDictionary wordsDictionary = Provider.of<WordsDictionary>(context);
-
-    return Container(
-        child: Card(
-      child: Padding(
-        padding: EdgeInsets.all(7),
-        child: Stack(children: <Widget>[
-          Align(
-              alignment: Alignment.centerRight,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  WordCard(),
-                  DoYouKnow(),
-                  GetUses(),
-                  ShowAnswer(),
-                  NextQuestion()
-                ],
-              ))
-        ]),
-      ),
-    ));
+    TestConfig testConfig = Provider.of<TestConfig>(context);
+    print('HERE');
+    print(wordsDictionary.wordForTest(testConfig.wordsAtTest));
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (context) => TestManeger(
+                wordsDictionary.wordForTest(testConfig.wordsAtTest)))
+      ],
+      child: Container(
+          child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(7),
+          child: Stack(children: <Widget>[
+            Align(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    WordCard(),
+                    DoYouKnow(),
+                    GetUses(),
+                    ShowAnswer(),
+                    NextQuestion()
+                  ],
+                ))
+          ]),
+        ),
+      )),
+    );
   }
 }
