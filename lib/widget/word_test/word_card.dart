bc122e5e810d4909b930015c24dda8ad4f415036
shaked chen <shaked1hen@gmail.com>
2020-08-23 12:23:21 +0300

    created the show word card

--- lib/widget/word_test/word_card.dart
+++ lib/widget/word_test/word_card.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/models/test_maneger.dart';
 
 class WordCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TestManeger testManeger = Provider.of<TestManeger>(context);
     return Container(
-      child: Card(child: Text('Test word'), color: Colors.blue[400]),
+      padding: EdgeInsets.all(12),
+      child: Card(
+          child: Container(
+            child: Text(testManeger.currentWord.word,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 35,
+                )),
+          ),
+          color: Colors.blue[100]),
     );
   }
 }
