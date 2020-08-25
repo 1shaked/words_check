0bc7e63912422254a27d0ca1b0c991f411740555
shaked chen <shaked1hen@gmail.com>
2020-08-25 10:43:11 +0300

    added the check for when finish test

--- lib/widget/word_test_body.dart
+++ lib/widget/word_test_body.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:word_me/models/test_config.dart';
 import 'package:word_me/models/test_maneger.dart';
-import 'package:word_me/models/words_dictionary.dart';
 import 'package:word_me/widget/word_test/do_know.dart';
 import 'package:word_me/widget/word_test/get_uses.dart';
 import 'package:word_me/widget/word_test/next_question.dart';
 import 'package:word_me/widget/word_test/show_answer.dart';
 import 'package:word_me/widget/word_test/word_card.dart';
+import 'package:word_me/widget/word_test/word_status.dart';
+import 'package:provider/provider.dart';
 
 class WordTestBody extends StatefulWidget {
   @override
@@ -15,22 +15,12 @@ class WordTestBody extends StatefulWidget {
 }
 
 class _WordTestBodyState extends State<WordTestBody> {
-  int currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    WordsDictionary wordsDictionary = Provider.of<WordsDictionary>(context);
-    TestConfig testConfig = Provider.of<TestConfig>(context);
-    print('HERE');
-    print(wordsDictionary.wordForTest(testConfig.wordsAtTest));
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-            create: (context) => TestManeger(
-                wordsDictionary.wordForTest(testConfig.wordsAtTest)))
-      ],
-      child: Container(
-          child: Card(
+    TestManeger testManeger = Provider.of<TestManeger>(context);
+
+    return Container(
+      child: Card(
         child: Padding(
           padding: EdgeInsets.all(7),
           child: Stack(children: <Widget>[
@@ -40,17 +30,25 @@ class _WordTestBodyState extends State<WordTestBody> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   mainAxisSize: MainAxisSize.max,
-                  children: [
-                    WordCard(),
-                    DoYouKnow(),
-                    GetUses(),
-                    ShowAnswer(),
-                    NextQuestion()
-                  ],
+                  children: testManeger.isDone
+                      ? [
+                          Text('YOU ARE DONE!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 35, fontWeight: FontWeight.bold))
+                        ] // TODO: add a done page
+                      : [
+                          WordStatus(),
+                          WordCard(),
+                          DoYouKnow(),
+                          GetUses(),
+                          ShowAnswer(),
+                          NextQuestion()
+                        ],
                 ))
           ]),
         ),
-      )),
+      ),
     );
   }
 }
