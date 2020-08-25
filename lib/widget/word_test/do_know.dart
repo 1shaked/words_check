1dc68d7b6830418c91a5a4c4224e8a44921d00ae
shaked chen <shaked1hen@gmail.com>
2020-08-25 10:41:42 +0300

    added the funcnality when you know word

--- lib/widget/word_test/do_know.dart
+++ lib/widget/word_test/do_know.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:word_me/const/test_conditions.dart';
+import 'package:word_me/models/test_maneger.dart';
+import 'package:word_me/models/words_dictionary.dart';
 
 class DoYouKnow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TestManeger testManeger = Provider.of<TestManeger>(context);
+    WordsDictionary wordsDictionary = Provider.of<WordsDictionary>(context);
     return Container(
       child: SizedBox(
         height: 40,
@@ -13,8 +19,18 @@ class DoYouKnow extends StatelessWidget {
             Expanded(
                 child: Container(
               color: Colors.black,
-              child: RaisedButton.icon(
-                  onPressed: null,
+              child: FlatButton.icon(
+                  onPressed: () {
+                    testManeger.testResultsAppend(test_unknow);
+                    if (testManeger.currentWord.score - 1 ==
+                        testManeger.currentWord.knownWordScore) {
+                      wordsDictionary.changeScore(testManeger.currentWord,
+                          testManeger.currentWord.unknownWordScore);
+                    } else {
+                      wordsDictionary.changeScore(testManeger.currentWord,
+                          testManeger.currentWord.score - 1);
+                    }
+                  },
                   icon: Icon(Icons.cancel, color: Colors.white),
                   label: Text("did'nt know",
                       style: TextStyle(color: Colors.white))),
@@ -22,8 +38,16 @@ class DoYouKnow extends StatelessWidget {
             Expanded(
                 child: Container(
               color: Colors.green,
-              child: RaisedButton.icon(
-                  onPressed: null,
+              child: FlatButton.icon(
+                  onPressed: () {
+                    testManeger.testResultsAppend(test_know);
+                    if (testManeger.currentWord.score !=
+                        testManeger.currentWord.knownWordScore) {
+                      wordsDictionary.changeScore(testManeger.currentWord,
+                          testManeger.currentWord.score + 1);
+                      testManeger.currentWord.score += 1;
+                    }
+                  },
                   icon: Icon(Icons.check_circle, color: Colors.white),
                   label: Text("did know!",
                       style: TextStyle(
