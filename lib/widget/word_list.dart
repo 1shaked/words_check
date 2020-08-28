44817d2af2449aed983c0c728b3df34227a76b4e
shaked chen <shaked1hen@gmail.com>
2020-08-28 15:37:53 +0300

    added the spin kit loader

--- lib/widget/word_list.dart
+++ lib/widget/word_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:word_me/models/words_dictionary.dart';
 import 'package:word_me/widget/word_list_item.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class WordList extends StatefulWidget {
   @override
@@ -12,20 +13,19 @@ class _WordListState extends State<WordList> {
   @override
   Widget build(BuildContext context) {
     WordsDictionary wordsDictionary = Provider.of<WordsDictionary>(context);
-    
-    if (wordsDictionary.currentDictionary  != null) {
+
+    if (wordsDictionary.isDoneLoading) {
       return Container(
         child: ListView(
-          children: wordsDictionary.currentDictionary.asMap().entries.map((entry) {
-            int idx = entry.key;
-            return WordListItem(entry.value, idx);
-          }).toList(),
-          shrinkWrap: true
-        ),
+            children:
+                wordsDictionary.currentDictionary.asMap().entries.map((entry) {
+              int idx = entry.key;
+              return WordListItem(entry.value, idx);
+            }).toList(),
+            shrinkWrap: true),
       );
     } else {
-      return Text('no data here');
+      return Container(child: const SpinKitRotatingCircle(color: Colors.white));
     }
   }
 }
-
