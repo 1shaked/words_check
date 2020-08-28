1f44ca763de37b2b42711c01f1012db11dcd7711
shaked chen <shaked1hen@gmail.com>
2020-08-28 16:51:26 +0300

    added if heck for the box

--- lib/widget/word_test/do_know.dart
+++ lib/widget/word_test/do_know.dart
@@ -12,50 +12,52 @@ class DoYouKnow extends StatelessWidget {
     return Container(
       child: SizedBox(
         height: 40,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-                child: Container(
-              color: Colors.black,
-              child: FlatButton.icon(
-                  onPressed: () {
-                    testManeger.testResultsAppend(test_unknow);
-                    if (testManeger.currentWord.score - 1 ==
-                        testManeger.currentWord.knownWordScore) {
-                      wordsDictionary.changeScore(testManeger.currentWord,
-                          testManeger.currentWord.unknownWordScore);
-                    } else {
-                      wordsDictionary.changeScore(testManeger.currentWord,
-                          testManeger.currentWord.score - 1);
-                    }
-                  },
-                  icon: Icon(Icons.cancel, color: Colors.white),
-                  label: Text("did'nt know",
-                      style: TextStyle(color: Colors.white))),
-            )),
-            Expanded(
-                child: Container(
-              color: Colors.green,
-              child: FlatButton.icon(
-                  onPressed: () {
-                    testManeger.testResultsAppend(test_know);
-                    if (testManeger.currentWord.score !=
-                        testManeger.currentWord.knownWordScore) {
-                      wordsDictionary.changeScore(testManeger.currentWord,
-                          testManeger.currentWord.score + 1);
-                      testManeger.currentWord.score += 1;
-                    }
-                  },
-                  icon: Icon(Icons.check_circle, color: Colors.white),
-                  label: Text("did know!",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ))),
-            )),
-          ],
-        ),
+        child: !testManeger.isAnswerdCurrentWord
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                      child: Container(
+                    color: Colors.black,
+                    child: FlatButton.icon(
+                        onPressed: () {
+                          testManeger.testResultsAppend(test_unknow);
+                          if (testManeger.currentWord.score - 1 ==
+                              testManeger.currentWord.knownWordScore) {
+                            wordsDictionary.changeScore(testManeger.currentWord,
+                                testManeger.currentWord.unknownWordScore);
+                          } else {
+                            wordsDictionary.changeScore(testManeger.currentWord,
+                                testManeger.currentWord.score - 1);
+                          }
+                        },
+                        icon: Icon(Icons.cancel, color: Colors.white),
+                        label: Text("did'nt know",
+                            style: TextStyle(color: Colors.white))),
+                  )),
+                  Expanded(
+                      child: Container(
+                    color: Colors.green,
+                    child: FlatButton.icon(
+                        onPressed: () {
+                          testManeger.testResultsAppend(test_know);
+                          if (testManeger.currentWord.score !=
+                              testManeger.currentWord.knownWordScore) {
+                            wordsDictionary.changeScore(testManeger.currentWord,
+                                testManeger.currentWord.score + 1);
+                            testManeger.currentWord.score += 1;
+                          }
+                        },
+                        icon: Icon(Icons.check_circle, color: Colors.white),
+                        label: Text("did know!",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ))),
+                  )),
+                ],
+              )
+            : Container(),
       ),
     );
   }
